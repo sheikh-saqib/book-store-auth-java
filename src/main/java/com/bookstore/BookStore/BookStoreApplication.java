bc3c686cff41fcffeea55f4e6fef9efb1db7f337
Sheikh Saqib <sheikhsaqib40@gmail.com>
2024-06-16 14:54:37 +0100

    Added Profiles

--- src/main/java/com/bookstore/BookStore/BookStoreApplication.java
+++ src/main/java/com/bookstore/BookStore/BookStoreApplication.java
@@ -1,13 +1,28 @@
 package com.bookstore.BookStore;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class BookStoreApplication {
 
+	@Autowired
+	private Environment environment;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		String[] activeProfiles = environment.getActiveProfiles();
+		System.out.println("Active Profiles: " + Arrays.toString(activeProfiles));
 	}
 
 }
